1b7c66c826aa5e472d2c6569bb6065567c74cedf
rahul-83 <63275013+rahul-83@users.noreply.github.com>
2020-09-11 20:01:42 +0530

    Update BaseChronometer.java

--- java/com/philliphsu/clock2/chronometer/BaseChronometer.java
+++ java/com/philliphsu/clock2/chronometer/BaseChronometer.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2017 Phillip Hsu
- *
- * This file is part of ClockPlus.
- *
- * ClockPlus is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ClockPlus is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ClockPlus.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 package com.philliphsu.clock2.chronometer;
 
@@ -27,12 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-/**
- * Created by Phillip Hsu on 9/9/2016.
- *
- * Based on the framework's Chronometer class. Can be configured as a countdown
- * chronometer and can also show centiseconds.
- */
 public class BaseChronometer extends TextView {
     private static final String TAG = "BaseChronometer";
 
